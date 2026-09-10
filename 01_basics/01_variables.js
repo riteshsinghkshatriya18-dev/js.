b05b5d7e012e29e412c8064703e96d94573ffcad
riteshsinghkshatriya18-dev <riteshsinghkshatriya18@gmail.com>
2026-09-10 13:51:44 +0530

    Add account variables and modify their values
    Initialize variables for account details and update values.

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId =144553
+let accountEmail = "ritesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Madhya Pradesh"
+
+// accountId =2 // not allowed
+
+accountEmail="hc@hc.com"
+accountPassword="323232"
+accountCity="jaipur"
+
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
