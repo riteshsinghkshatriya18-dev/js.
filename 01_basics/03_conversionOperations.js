fa5aaf4d0d28db2734caf1ae681a4483c24903ff
riteshsinghkshatriya18-dev <riteshsinghkshatriya18@gmail.com>
2026-09-11 12:03:06 +0000

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,58 @@
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// // "33"=> 33
+// //"33abc" => NaN
+// // true => 1; false =>0
+
+// let isLoggedIn =1
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// let somenumber= 33
+
+// let stringnumber = String(somenumber)
+// console.log(stringnumber)
+// console.log(typeof stringnumber)
+
+//******************OPERATIONS********* */
+
+let value =3
+let negvalue = -value
+//console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 ="hitesh"
+// let str2= " hello"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// num1=num2=num3 =2+2
+
+let gamecounter =100
+++gamecounter;
+console.log(gamecounter);
