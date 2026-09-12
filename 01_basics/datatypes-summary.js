12155492b376ddefe76d266d386b0c08f0ee3413
riteshsinghkshatriya18-dev <riteshsinghkshatriya18@gmail.com>
2026-09-12 17:11:09 +0000

    datatypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -30,3 +30,26 @@ const myfunction=function(){
     console.log("Hello world")
 }
 console.log(typeof bigNumber)
+
+//+++++++++++++++++++++++++
+
+// Stack(Primitive), Heap(Non-Primitive)
+
+let myYoutubename="hiteshchoudharydotcom"
+
+let anothername = myYoutubename
+
+anothername ="chaiaurcode"
+console.log(anothername)
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
